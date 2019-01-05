611c010cdd5e121ea3c70310208f6688b333ee22
Ruben Castro <ruben.jose.castro@gmail.com>
2019-01-05 13:26:49 -0500

    Other Updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { RecipeListComponent } from './recepies/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recepies/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recepies/recipe-list/recipe-item/recipe-item.component';
+import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { ShopingEditComponent } from './shoping-list/shoping-edit/shoping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecepiesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShopingListComponent,
+    ShopingEditComponent
   ],
   imports: [
     BrowserModule
