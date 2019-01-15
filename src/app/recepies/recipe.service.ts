ff222db7395c9941ebc5f5478b1ffbf81ac418f4
Ruben Castro <ruben.jose.castro@gmail.com>
2019-01-15 09:47:00 -0500

    Update using the Services

--- src/app/recepies/recipe.service.ts
+++ src/app/recepies/recipe.service.ts
@@ -1,7 +1,9 @@
+import {EventEmitter} from '@angular/core';
 import {Recipe} from './recipe.model';
 
 export class RecipeService {
-  recipes: Recipe[] = [
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
     new Recipe('A test Recipe','This is a simply test','https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg'),
     new Recipe('Another Recipe','This is a simply test','https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg')
   ];
