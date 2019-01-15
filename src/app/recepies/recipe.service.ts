0f291d69535185a674fe498662e26ca5c62ff5e7
Ruben Castro <ruben.jose.castro@gmail.com>
2019-01-15 09:03:44 -0500

    Updates using Even Emit

---
+++ src/app/recepies/recipe.service.ts
@@ -0,0 +1,12 @@
+import {Recipe} from './recipe.model';
+
+export class RecipeService {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe','This is a simply test','https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg'),
+    new Recipe('Another Recipe','This is a simply test','https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice(); //
+  }
+}
